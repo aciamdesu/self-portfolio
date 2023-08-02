fe27d5af8e0fcfdf8cbd49866adf6d83f88eba63
aciamdesu <shalomjamaicaave@gmail.com>
2023-08-02 18:39:05 +0800

    setup

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx';
+import Project from './pages/Project/Project.jsx';
+import Archive from './pages/Archive/Archive.jsx';
+import Contact from './pages/Contact/Contact.jsx';
+import Root from './layout/root/index.jsx';
+
+const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/About',
+                element: <About/>
+            },
+            {
+                path: '/Project',
+                element: <Project/>
+            },
+            {
+                path: '/Archive',
+                element: <Archive/>
+            },
+            {
+                path: '/Contact',
+                element: <Contact/>
+            }
+        ]
+    }
+])
+
+function App () {
+    return <RouterProvider Router={ Router }/>;
+}
+
+export default App
