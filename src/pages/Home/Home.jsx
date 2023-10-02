063dee4efa72cbe518de44b81b40642786c6b63a
aciamdesu <shalomjamaicaave@gmail.com>
2023-10-02 12:46:02 +0800

    initial home-page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,140 +1,24 @@
-
+import { Link } from 'react-router-dom'
+import './Home.css'
 
 const Home = () => {
     return(
-        <main>
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            v
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
-            Hello world
+        <main className="home__content-wrapper">
+            <div className="home__header-wrapper">
+                <div className="home__header-upper">
+                    <p>Hi and welcome to my world!</p>
+                    <p>My name is <span className="home__header-color">Shalom Jamaica Ave</span>.</p>
+                </div>
+                <div className="home__header-lower">
+                    <p>A <span className="home__header-color">creative</span> frontend developer currently</p>
+                    <p>a college student.</p>
+                </div>
+            </div>
+            <div className="home__button-wrapper">
+                <button className="home__resume-btn">
+                    RESUME
+                </button>
+            </div>
         </main>
     )
 }
